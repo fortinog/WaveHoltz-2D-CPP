5937ad1c49d38aff03b99ddf2917f395fef83013
Fortino Garcia <fortino.garcia@colorado.edu>
2020-04-07 10:56:16 -0600

    Cleaned up main.cpp and added a gitignore file.

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,11 @@ using namespace std::chrono;
 
 int main()
 {
-    Darray2 u, v, res;
-    double err;
-    vector<int> size_vec(5);
-    int start_size = 32;
-    int N,M;
-    int nsteps;
-    double energy = 0.0;
-    double energy_old = 0.0;
-
     Wave_Solve solver;
-    nsteps = solver.setup.nsteps;
     solver.Solve_PDE();
 
     cout << "The computed errors are: " << "\n";
+    
     // Compute error
     cout << scientific << right << setw(14)<< solver.Compute_Solution_Error(1);
     cout << scientific << right << setw(14)<< solver.Compute_Solution_Error(2);
